75b01e7a965ef1b20ae840a6151047de830abca0
Cary Zhou <caryz@terpmail.umd.edu>
2017-07-30 20:00:47 -0400

    cleaned up scheduleTabStoryboard

--- guide-for-android-summit/ScheduleTableViewController.swift
+++ guide-for-android-summit/ScheduleTableViewController.swift
@@ -35,15 +35,8 @@ class ScheduleTableViewController: UIViewController, UITableViewDelegate, UITabl
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         configureTableView()
         fetchSchedule()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     func configureTableView() {
