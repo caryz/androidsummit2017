3e22c81729747967a6aa013e6637a8644ede7782
Cary Zhou <caryz@terpmail.umd.edu>
2017-08-10 21:06:23 -0400

    fixed tab bar animation flicker and made navigation bars not translucent

--- guide-for-android-summit/AnimatedTabBarController.swift
+++ guide-for-android-summit/AnimatedTabBarController.swift
@@ -23,6 +23,7 @@ class AnimatedTabBarController: UITabBarController, UITabBarControllerDelegate {
         let fromView = selectedViewController!.view
         let toView = viewController.view
 
+        fromView!.superview?.addSubview(toView!) // avoid nav bar flicker
         UIView.transition(from: fromView!, to: toView!, duration: 0.5, options: [.transitionCrossDissolve], completion: nil)
         
         return true
